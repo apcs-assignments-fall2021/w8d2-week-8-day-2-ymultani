676ae304ea5b4491ac2f60a604a008bdb63984b0
yuvro <23ymultani@athenian.org>
2021-10-20 17:19:29 -0700

    We got that trophy like a champion, fell asleep on the jet, we gone.

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -26,33 +26,66 @@ public class Rational {
     // (When you write the simplify method later on, you should
     // also call it in this method to return the simplified result)
     public static Rational add(Rational r, Rational s) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int num1 = r.numerator;
+        int den1 = r.denominator;
+        int num2 = s.numerator;
+        int den2 = s.denominator;
+        int newnum1 = num1 * den2;
+        int newnum2 = num2 * den1;
+        int newden1 = den1 * den2;
+        int newden2 = newden1;
+        int numsum = newnum1 + newnum2;
+        Rational sum = new Rational(numsum, newden2);
+        return simplify(sum);
+
+
+
+
+
     }
 
     // This method takes two Rationals, subtracts thems up, 
     // and returns a Rational equal to the difference
     public static Rational subtract(Rational r, Rational s) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int num1 = r.numerator;
+        int den1 = r.denominator;
+        int num2 = s.numerator;
+        int den2 = s.denominator;
+        int newnum1 = num1 * den2;
+        int newnum2 = num2 * den1;
+        int newden1 = den1 * den2;
+        int newden2 = newden1;
+        int numsum = newnum1 - newnum2;
+        Rational sum = new Rational(numsum, newden2);
+        return simplify(sum);
     }
     
     public static Rational multiply(Rational r, Rational s) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int num = r.numerator * s.numerator;
+        int den = r.denominator * s.denominator;
+        Rational multiple = new Rational(num,den);
+        return simplify(multiple);
     }
     
     public static Rational divide(Rational r, Rational s) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int num = r.numerator * s.denominator;
+        int den  = r.denominator * s.numerator;
+        Rational quotient = new Rational(num,den);
+        return simplify(quotient);
     }
 
     // Finds the greatest common factor between a and b
     // To find the greatest common factor, find the largest number x
     // such that a and b are both multiples of x
     public static int greatestCommonFactor(int a, int b){
-        // REPLACE WITH YOUR CODE HERE
-        return 1;
+        int x = 1;
+        for(int i = Math.min(a,b); i>= 1; i--){
+            if (a%i == 0 && b%i == 0){
+                x = i;
+                break;
+            }
+        }
+        return x;
     }
 
     // This method is given a rational, and returns a simplified version
@@ -61,8 +94,11 @@ public class Rational {
     // e.g. simplify(2/4) => 1/2
     //      simplify(1/2) => 1/2
     public static Rational simplify(Rational r) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int divisor = greatestCommonFactor(r.numerator,r.denominator);
+        int new_num = r.numerator/divisor;
+        int new_den = r.denominator/divisor;
+        Rational simplified = new Rational(new_num,new_den);
+        return simplified;
     }
 
     // This following method is NOT static, we'll talk about it next class!
--- src/main/java/RationalClient.java
+++ src/main/java/RationalClient.java
@@ -10,15 +10,15 @@ public class RationalClient {
         System.out.println(r.denominator); // 2
 
 //        // A basic test for adding Rationals
-//        Rational s = new Rational(1, 3);
-//        System.out.println(s); // 1/3
-//        Rational t = Rational.add(r, s);
-//        System.out.println(t); // 5/6
+        Rational s = new Rational(1, 3);
+        System.out.println(s); // 1/3
+        Rational t = Rational.divide(r, s);
+        System.out.println(t); // 5/6
 //
-//        // A more advanced test for adding Rational that won't work
+        //A more advanced test for adding Rational that won't work
 //        // until the greatestCommonFactor and simplify methods work
-//        Rational x = new Rational(1, 6);
-//        Rational y = Rational.add(t, x);
-//        System.out.println(y); // 1/1
+        Rational x = new Rational(1, 6);
+        Rational y = Rational.add(t, x);
+        System.out.println(Rational.simplify(y)); // 1/1
     }
 }
